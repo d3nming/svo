cf63e0fa9ca0a1fb13d0f803f05711cff1f77714
Dantu <Dantu@DESKTOP-C23MO52>
2024-04-18 15:37:26 +0300

    Добавьте файлы проекта.

---
+++ svo/View/Add.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace svo.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Window
+    {
+        public Add()
+        {
+            InitializeComponent();
+        }
+        Model1 app = new Model1();
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            int? d = Convert.ToInt32(Day.Text);
+            int? dd = Convert.ToInt32(Day.Text);
+            DateTime datepick = DatePick.SelectedDate ?? DateTime.Now;
+            Activity newItem = new Activity
+            {
+                NameEvent = NameEvent.Text,
+                DateStart = datepick,
+                Day = d,
+                Days = dd
+            };
+            app.Activity.Add(newItem);
+            app.SaveChanges();
+            this.Close();
+        }
+    }
+}
---
+++ svo/View/Okno.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace svo.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Okno.xaml
+    /// </summary>
+    public partial class Okno : Window
+    {
+        Model1 app = new Model1();
+        List<Activity> activities;
+        public Okno()
+        {
+            InitializeComponent();
+            bib();
+        }
+        public void bib()
+        {
+            activities = app.Activity.ToList();
+            ProductList.ItemsSource = activities;
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            Add s = new Add();
+            s.ShowDialog();
+            ProductList.ItemsSource = null;
+            bib();
+        }
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            int Id = Convert.ToInt32(btn.Tag.ToString());
+            var akt = app.Activity.FirstOrDefault(a => a.Id == Id);
+            app.Activity.Remove(akt);
+            app.SaveChanges();
+            MessageBox.Show("Delete");
+            bib();
+        }
+    }
+}
